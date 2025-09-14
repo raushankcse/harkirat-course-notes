f1f5ff0199c9df133aede8726caabcc639df00c1
Raushan Kumar <raushankcse@gmail.com>
2025-09-14 14:48:02 +0530

    database

--- week7/p1/index.js
+++ week7/p1/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose")
+const JWTSECRET = "hellofolks"
+const {UserModel, TodoModel} = require("./db")
 
-
+mongoose.connect("mongodb+srv://raushankcse_db_user:01032003@cluster0.igfpjot.mongodb.net/todo");
 
 const app = express();
 
@@ -8,22 +12,112 @@ app.use(express.json());
 
 
 
-app.post("/signup", (req, res)=>{
+app.post("/signup", async (req, res)=>{
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+
+  await UserModel.create({
+    name: name,
+    email: email,
+    password: password
+  })
+
+
+  res.json({
+    message : "you are signed up!"
+  })
 
 });
 
 
-app.post("/signin", (req, res)=>{
+app.post("/signin", async (req, res)=>{
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await UserModel.findOne({
+    email: email,
+    password: password
+  })
+
+  if(user){
+    const token = jwt.sign({
+      id: user._id,
+    }, JWTSECRET);
+    res.status(200).json({
+      message: "user logged in",
+      token: token
+    })
+  }else{
+    res.status(403).json({
+      message: "incorrect credentials"
+    })
+  }
+
+});
+
+function auth(req, res, next){
+  
+  const token = req.headers.authorization;
+  if(!token){
+    res.status(403).json({
+      message: "sign in again, token expired!"
+    })
+  }
+  try {
+    const decodeToken = jwt.verify(token, JWTSECRET);
+    if(decodeToken){
+      req.id = decodeToken.id;
+      next();
+    }
+  } catch (error) {
+    res.status(403).json("Unauthorized users");
+  }
+}
+
+app.post("/todo", auth, async (req, res)=>{
+
+  const userId = req.id;
+  const title = req.body.title;
+  const done = req.body.done;
+
+  await TodoModel.create({
+    title: title,
+    done: done,
+    userId: userId
+  })
+
+  res.status(200).json({
+    message: "todo created"
+  })
+
+
 
 });
 
 
-app.post("/todo", (req, res)=>{
+app.get("/todos",auth, async (req,res)=>{
 
-});
+  const userId = req.id;
 
+  const todos = await TodoModel.find({
+    userId: userId
+  })
+  
 
-app.get("/todos",(req,res)=>{
+  if(todos.length>0){
+    res.json({
+      todos: todos
+    })
+  }
+
+  res.json({
+    message: "no todos found"
+  })
+
 
 });
 
