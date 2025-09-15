656916afea26b640a8c62cd0e57789dfe5b346ce
Raushan Kumar <raushankcse@gmail.com>
2025-09-15 14:26:30 +0530

    routing

--- week8/course-selling/index.js
+++ week8/course-selling/index.js
@@ -1,3 +1,21 @@
 const express = require("express")
 
+const app = express();
+const {userRouter} = require("./routes/user");
+const {courseRouter} = require("./routes/course");
 
+
+
+
+app.use("/user", userRouter);
+app.use("/course", courseRouter);
+
+
+
+
+
+
+
+
+
+app.listen(3000);
---
+++ week8/course-selling/routes/course.js
@@ -0,0 +1,22 @@
+const { Router } = require("express")
+
+const courseRouter = Router();courseRouter
+
+
+courseRouter.post("/purchase",(req, res)=>{
+  res.json({
+    message: "signup endpoint"
+  })
+})
+
+
+courseRouter.get("/preview",(req, res)=>{
+  res.json({
+    message: "signup endpoint"
+  })
+})
+
+
+module.exports  = {
+  courseRouter: courseRouter
+}
