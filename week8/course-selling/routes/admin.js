47e9d2b7b0e319bac0a5c40d7aea06ec4eaf0c87
Raushan Kumar <raushankcse@gmail.com>
2025-09-15 14:34:35 +0530

    admin

--- week8/course-selling/routes/admin.js
+++ week8/course-selling/routes/admin.js
@@ -25,6 +25,19 @@ adminRouter.post("/course",(req, res)=>{
 })
 
 
+adminRouter.put("/course",(req, res)=>{
+  res.send({
+    message: "admin end point"
+  })
+})
+
+adminRouter.get("/course/bulk",(req, res)=>{
+  res.send({
+    message: "admin end point"
+  })
+})
+
+
 
 
 
