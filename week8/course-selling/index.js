b146c17f105574ab353dbecb2276bea3d25a0f51
Raushan Kumar <raushankcse@gmail.com>
2025-09-15 14:33:32 +0530

    admin end point

--- week8/course-selling/index.js
+++ week8/course-selling/index.js
@@ -3,13 +3,15 @@ const express = require("express")
 const app = express();
 const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 
 
 
 
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
 
+app.use("api/v1/user", userRouter);
+app.use("api/v1/course", courseRouter);
+app.use("api/v1/admin", adminRouter);
 
 
 
---
+++ week8/course-selling/routes/admin.js
@@ -0,0 +1,42 @@
+const {Router } = require("express")
+const adminRouter = Router();
+
+
+adminRouter.post("/signup",(req, res)=>{
+  res.send({
+    message: "admin end point"
+  })
+})
+
+
+adminRouter.post("/signin",(req, res)=>{
+  res.send({
+    message: "admin end point"
+  })
+})
+
+
+adminRouter.use(adminMiddleware);
+
+adminRouter.post("/course",(req, res)=>{
+  res.send({
+    message: "admin end point"
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  adminRouter: adminRouter
+}
